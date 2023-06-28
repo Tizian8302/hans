732bc842843221c0512e78dd61c5f6b7ddd1f32e
tiian8302 <tizian2002@outlook.com>
2023-06-28 11:19:28 +0200

    moving

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import crypto from "crypto";
 import { newOrderRequestBody } from '../shared/types';
+import { JsonDB, Config } from 'node-json-db';
+
 const app = express();
 const port = 3000
 
 app.use(cors())
 app.use(express.static('public'))
+
+const db = new JsonDB(new Config("../data/hansDB", true, false, '/'))
+
+
 
 app.get('/api', (req, res) => {
   res.send('Hello World!')
@@ -29,17 +36,23 @@ app.get('/api/products', (req, res) => {
     ])
 })
 
-app.post('/api/orders', (req, res) => {
+app.post('/api/orders', async (req, res) => {
     const { name, wohnhaus, datum } = req.body as newOrderRequestBody;
   
+    // TODO: Perform any necessary validation or processing of the order data
     if (!name || !wohnhaus || !datum) {
         res.status(400).json({ message: "Bad Request"});
         return;
     }
-    // TODO: Perform any necessary validation or processing of the order data
   
+    const dbentry = {
+        id: crypto.randomUUID(),
+        name,
+        wohnhaus,
+        datum,
+        Array
+    }
     // TODO: Save the order to your database or perform any other required actions
-  
     // Return a success response
     res.status(201).json({ message: 'Order created successfully' });
 });
