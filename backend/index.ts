1a4db9404631e19a72cf693b6cbf9eb5ac0a73b8
tiian8302 <tizian2002@outlook.com>
2023-06-28 10:43:12 +0200

    moved backend

---
+++ backend/index.ts
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors');
+
+app.use(cors())
+app.use(express.static('public'))
+
+app.get('/api', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/api/products', (req, res) => {
+    res.send([
+        {
+            name: 'Apfel',
+            type: 'Frucht',
+            price: 1.5,
+            manufacturer: 'Migros',
+            quantityType: 'Stk'
+        }, {
+            name: 'Birne',
+            type: 'Frucht',
+            price: 1.2,
+            manufacturer: 'Coop',
+            quantityType: 'Stk'
+        }
+    ])
+})
+
+app.post('/api/orders', (req, res) => {
+    const { name, wohnhaus, datum } = req.body;
+  
+    // TODO: Perform any necessary validation or processing of the order data
+  
+    // TODO: Save the order to your database or perform any other required actions
+  
+    // Return a success response
+    res.status(201).json({ message: 'Order created successfully' });
+});
+  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
