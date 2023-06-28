5cab0537254c51a58a070ac1a09997552e606a68
tiian8302 <tizian2002@outlook.com>
2023-06-28 10:49:20 +0200

    removed node modules

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
+import express from 'express';
+import cors from 'cors';
+const app = express();
 const port = 3000
-const cors = require('cors');
 
 app.use(cors())
 app.use(express.static('public'))
