1f0835eef6be3b7fe0e6b3434d2d41f152e8fdf0
tiian8302 <tizian2002@outlook.com>
2023-07-20 07:58:40 +0200

    deploying netlify and cyclic

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const cors_1 = __importDefault(require("cors"));
 const crypto_1 = __importDefault(require("crypto"));
 const node_json_db_1 = require("node-json-db");
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use((0, cors_1.default)());
 app.use(express_1.default.static('public'));
 app.use(express_1.default.json());
--- backend/index.ts
+++ backend/index.ts
@@ -5,7 +5,7 @@ import { DBOrder, newOrderRequestBody, newProductRequestBody } from '../shared/t
 import { JsonDB, Config } from 'node-json-db';
 
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.static('public'))
