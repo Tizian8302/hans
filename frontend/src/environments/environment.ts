62fee1bc4cc80c6789d90551b130e72e74296fdb
Tizi <myemail@gmail.com>
2023-07-29 16:44:16 +0200

    new url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     password: 'Hans8302',
     adminPassword: "AdminHans8302",
-    apiUrl: "https://hans-inventory-pqfpo4mzoa-oa.a.run.app/api"
+    apiUrl: "https://hans-pqfpo4mzoa-oa.a.run.app/api"
 };
